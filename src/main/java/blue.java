9ec01cd0bd8821cb561697716e29806690ebc143
anhbv1 <root@ip-172-31-27-242.ec2.internal>
2017-03-16 11:36:10 +0000

    ms

---
+++ src/main/java/blue.java
@@ -0,0 +1,11 @@
+/**
+ * Created by AnhBV1 on 3/16/2017.
+ */
+public class blue {
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World");
+    }
+
+}
